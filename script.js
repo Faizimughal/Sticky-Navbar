cd5e5f589b2961580fdca7eb236ecd00cd7f3d7b
Faizimughal <162406605+Faizimughal@users.noreply.github.com>
2024-11-28 00:34:55 +0500

    changes

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let newDate = new Date().getFullYear();
 date.innerHTML = newDate;
 let hambarg = document.getElementById("hambarg");
 let toggleNavbar = document.getElementById("toggle-navbar");
+window.addEventListener('DOMContentLoaded',()=>{
+    toggleNavbar.style.display = "none";
+})
 hambarg.addEventListener("click", () => {
   toggleNavbar.style.display =
     toggleNavbar.style.display == "none" ? "block" : "none";
